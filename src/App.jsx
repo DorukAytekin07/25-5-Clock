fc3977b95a1a8830beb443b692346fe1df5e309a
Doruk Aytekin <horationelson1903@gmail.com>
2023-10-13 23:52:30 -0700

    publish website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Clock from './Clock'
+import Footer from './Footer'
+function App() {
+  return(
+    <div>
+      <Clock></Clock>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
